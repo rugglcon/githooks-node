07a12f09a5f0b853d94068042ba9b2889d3533cb
Connor Ruggles <conruggles@gmail.com>
2019-09-05 01:22:48 -0500

    try some other stuff

--- index.js
+++ index.js
@@ -1,34 +1,22 @@
 const express = require('express');
-const exec = require('child_process').exec;
+const exec = require('child_process').execSync;
+const npmRun = require('npm-run');
 
 const app = express();
 
 app.post('/connorruggles.dev', (req, res) => {
     console.log(`received webhook for connorruggles.dev from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec(`cd /var/www/connorruggles.dev/html && git pull`, (err, stdout) => {
-        if (err) {
-            console.log('an error occurred with connorruggles.dev deploy');
-            console.log(err);
-            res.status(500).send(err);
-        }
-        console.log(stdout);
-        console.log('successfully deployed connorruggles.dev');
-        res.end();
-    });
+    exec(`cd /var/www/connorruggles.dev/html && git pull`);
+    console.log('successfully deployed connorruggles.dev');
 });
 
 app.post('/budget-tracker-ui', (req, res) => {
     console.log(`received webhook for budget-tracker-ui from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec('cd /home/connor/dev/budget-tracker-ui && npm run build:prod && cp dist/* /var/www/budget-tracker-ui', (err, stdout) => {
-        if (err) {
-            console.log('an error occurred with budget-tracker-ui deploy');
-            console.log(err);
-            res.status(500).send(err);
-        }
-        console.log(stdout);
-        console.log('successfully deployed budget-tracker-ui');
-        res.end();
-    });
+    exec('cd /home/connor/dev/budget-tracker-ui && git pull');
+    npmRun.execSync('npm run build:prod', {cwd: '/home/connor/dev/budget-tracker-ui'});
+    exec('cp dist/* /var/www/budget-tracker-ui');
+    console.log('successfully deployed budget-tracker-ui');
+    res.end();
 });
 
 app.listen(3300);
