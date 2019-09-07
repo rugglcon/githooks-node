18b2254197a6aeadd4065dbac2b5e623d5cdb6fa
Connor Ruggles <conruggles@gmail.com>
2019-09-07 00:18:11 -0500

    yayyy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.post('/budget-tracker-ui', (req, res) => {
     console.log(`received webhook for budget-tracker-ui from host: ${req.headers.host}, origin: ${req.get('origin')}`);
     exec('cd /home/connor/dev/budget-tracker-ui && git pull && npm install');
     exec('./node_modules/.bin/ng build --prod --progress=false', {cwd: '/home/connor/dev/budget-tracker-ui'});
-    exec('cp dist/* /var/www/budget-tracker-ui');
+    exec('/bin/cp dist/* /var/www/budget-tracker-ui');
     console.log('successfully deployed budget-tracker-ui');
     res.end();
 });
