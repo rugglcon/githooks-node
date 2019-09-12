8f9ef6bd7608d259d1e4be03e6f5e0c095987e9a
Connor Ruggles <conruggles@gmail.com>
2019-09-11 23:37:50 -0500

    poosh

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ app.post('/budgettracker', (req, res) => {
 app.post('/githooks', (req, res) => {
     console.log(req.body);
     if (req.body.ref) console.log(req.body.ref);
+    else console.log('no ref :(');
     console.log(`received webhook for githooks-node from host: ${req.headers.host}, origin: ${req.get('origin')}`);
     exec('cd /var/www/githooks-node && git pull && npm install && /bin/systemctl restart githooks.service', (err, stdout) => {
         if (err) {
