0b39f2ad995583221d7d96cb5e63f0e34d89f598
Connor Ruggles <conruggles@gmail.com>
2020-03-26 23:06:32 -0500

    change deploy process

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const sendEmail = (subject, content) => {
     return new Promise((resolve, reject) => {
         mg.messages().send({
             from: process.env.EMAIL,
-            to: 'conruggles@gmail.com',
+            to: 'connor@connorruggles.dev',
             subject,
             text: content
         }, (err, body) => {
@@ -81,11 +81,11 @@ const sendEmail = (subject, content) => {
     });
 };
 
-const onError = (appName, err) => {
+const onError = (appName, err, stdout) => {
     console.log('something went wrong deploying ' + appName);
     console.log(err);
     return sendEmail('Error deploying ' + appName,
-                    `There was an error trying to deploy ${appName}: ${err.message}\n${err.stack}`)
+                    `There was an error trying to deploy ${appName}: ${err.message}\n${err.stack}.\nstdout: ${stdout}`)
             .then(() => console.log('sent email telling of error'))
             .catch(console.log);
 };
@@ -98,7 +98,7 @@ const onSuccess = appName => {
 
 app.post('/connorruggles.dev', (req, res) => {
     console.log(`received webhook for connorruggles.dev from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec(`cd /var/www/connorruggles.dev/html && git pull`, (err, stdout) => {
+    exec('git pull', { cwd: '/var/www/connorruggles.dev/html' }, (err, stdout) => {
         if (err) {
             return onError('connorruggles.dev', err);
         }
@@ -109,9 +109,7 @@ app.post('/connorruggles.dev', (req, res) => {
 
 app.post('/budget-tracker-ui', (req, res) => {
     console.log(`received webhook for budget-tracker-ui from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec('cd /home/connor/dev/budget-tracker-ui && git pull && ' +
-        'npm install && ./node_modules/.bin/ng build --prod --progress=false && ' +
-        '/bin/cp -R dist/budget-tracker/* /var/www/budget-tracker-ui', { cwd: '/home/connor/dev/budget-tracker-ui' }, (err, stdout) => {
+    exec('git pull && ./deploy.sh', { cwd: '/home/connor/dev/budget-tracker-ui' }, (err, stdout) => {
             if (err) {
                 return onError('budget-tracker-ui', err);
             }
@@ -122,9 +120,9 @@ app.post('/budget-tracker-ui', (req, res) => {
 
 app.post('/budgettracker', (req, res) => {
     console.log(`received webhook for budget tracker backend from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec('cd /var/www/budgettracker && git pull && npm install && ./node_modules/.bin/tsc && /bin/systemctl restart budget_api.service', { cwd: '/var/www/budgettracker' }, (err, stdout) => {
+    exec('git pull && ./deploy.sh', { cwd: '/var/www/budgettracker' }, (err, stdout) => {
         if (err) {
-            return onError('budgettracker', err);
+            return onError('budgettracker', err, stdout);
         }
         onSuccess('budgettracker');
     });
@@ -133,7 +131,7 @@ app.post('/budgettracker', (req, res) => {
 
 app.post('/githooks', (req, res) => {
     console.log(`received webhook for githooks-node from host: ${req.headers.host}, origin: ${req.get('origin')}`);
-    exec('cd /var/www/githooks-node && git pull && npm install && /bin/systemctl restart githooks.service', (err, stdout) => {
+    exec('git pull && npm install && /bin/systemctl restart githooks.service', { cwd: '/var/www/githooks-node' }, (err, stdout) => {
         if (err) {
             return onError('githooks', err);
         }
